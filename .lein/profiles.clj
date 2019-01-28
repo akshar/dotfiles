a986e09eb953ec0e129eac995f877ada529fef6a
Akshar Dave <akshar314@gmail.com>
2019-01-28 23:41:51 +0530

    add profile.clj

---
+++ .lein/profiles.clj
@@ -0,0 +1,7 @@
+{:user { :dependencies [[pjstadig/humane-test-output "0.8.3"]]
+        :injections [(require 'pjstadig.humane-test-output)
+                     (pjstadig.humane-test-output/activate!)]
+        :plugins [[cider/cider-nrepl "0.12.0"]
+                  [lein-try "0.4.3"]
+                  [lein-ancient "0.6.15"]
+                  [lein-pprint "1.2.0"]]}}
